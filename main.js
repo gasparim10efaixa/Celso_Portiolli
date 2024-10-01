0f56723a04e33796210d44efb88866c6435c4c28
gasparim <143743544+gasparim10efaixa@users.noreply.github.com>
2024-10-01 11:44:08 -0300

    eearar

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-function soma(num1,num2){
-    return num1+num2;
-}
-console.log(soma(3,4));
+// function soma(num1,num2){
+//     return num1+num2;
+// }
+// console.log(soma(3,4));
 
-function area(num1){
-    return num1**2;
-}
-console.log(area(4))
+// function area(num1){
+//     return num1**2;
+// }
+// console.log(area(4))
 
-let areaTrapézio= function(num1,num2,num3) {
-    return [num1+num2]*num3/2;
+// let areaTrapézio= function(num1,num2,num3) {
+//     return [num1+num2]*num3/2;
+// }
+// console.log(areaTrapézio(3,4,6));
+let nomes = ['Lucas','Joao P','Lorena','Maria','Arthur','Caio','Felipe B','Luiz','Nicolas','Julia K','Monise','Mel','Marina','Vinícius','Rhianne','João V','Laís','Gabriel','Aaron','Danilo']
+
+let i=0;
+while (i<10) {
+    console.log(i);
+    i=i+1;
 }
-console.log(areaTrapézio(3,4,6));
