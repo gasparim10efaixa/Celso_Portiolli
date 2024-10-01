763af2bbe4cb69624c0373823f9e9df2f265d156
gasparim <143743544+gasparim10efaixa@users.noreply.github.com>
2024-10-01 11:53:27 -0300

    21ddf

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 //     return [num1+num2]*num3/2;
 // }
 // console.log(areaTrapézio(3,4,6));
-let nomes = ['Lucas','Joao P','Lorena','Maria','Arthur','Caio','Felipe B','Luiz','Nicolas','Julia K','Monise','Mel','Marina','Vinícius','Rhianne','João V','Laís','Gabriel','Aaron','Danilo']
+let nomes = ['Lucas','Joao P','Lorena','Neymar Jr','Maria','Arthur','Caio','Felipe B','Luiz','Nicolas','Julia K','Monise','Mel','Marina','Vinícius','Rhianne','João V','Laís','Gabriel','Aaron','Danilo']
 
 let i=0;
 while (i<nomes.length) {
