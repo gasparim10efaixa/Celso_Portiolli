70d5e7d3f7f64a552fe139b9da6b454306123d79
gasparim <143743544+gasparim10efaixa@users.noreply.github.com>
2024-10-01 11:52:55 -0300

    fefwfw45

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
 let nomes = ['Lucas','Joao P','Lorena','Maria','Arthur','Caio','Felipe B','Luiz','Nicolas','Julia K','Monise','Mel','Marina','Vinícius','Rhianne','João V','Laís','Gabriel','Aaron','Danilo']
 
 let i=0;
-while (i<20) {
+while (i<nomes.length) {
     console.log(i);
     console.log(nomes[i]);
     i=i+1;
